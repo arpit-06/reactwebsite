e822f4b874dcc2107d6e2f14a92b78bcf099f4c9
arpit-06 <60512088+arpit-06@users.noreply.github.com>
2020-07-26 19:52:36 +0530

    Updates

---
+++ precache-manifest.f8d99eb88ea50e3117d420a6faaa9d5f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943ab722005d6e56061fee8134596593",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "8bfa36fe350ab89ae882",
+    "url": "/reactwebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "03117ec98472e604bf1d",
+    "url": "/reactwebsite/static/css/main.934055dd.chunk.css"
+  },
+  {
+    "revision": "8bfa36fe350ab89ae882",
+    "url": "/reactwebsite/static/js/2.012eb423.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebsite/static/js/2.012eb423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03117ec98472e604bf1d",
+    "url": "/reactwebsite/static/js/main.1cf7a6a7.chunk.js"
+  },
+  {
+    "revision": "0bb169a3a757acaebf1e",
+    "url": "/reactwebsite/static/js/runtime-main.d6619741.js"
+  },
+  {
+    "revision": "36c63226f08a8d1c29eba2d061c5a7e3",
+    "url": "/reactwebsite/static/media/img1.36c63226.svg"
+  },
+  {
+    "revision": "20efdd175e68c98cb8a5ab0e09668bf2",
+    "url": "/reactwebsite/static/media/img2.20efdd17.svg"
+  }
+]);
