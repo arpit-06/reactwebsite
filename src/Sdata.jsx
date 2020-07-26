f63439f5609796e84bffbc0c6d78a584c3efb29b
arpit-06 <60512088+arpit-06@users.noreply.github.com>
2020-07-26 19:18:31 +0530

    'Last'

---
+++ src/Sdata.jsx
@@ -0,0 +1,27 @@
+const Sdata = [
+    {
+        imgsrc: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        title: "Web Development",
+    },
+    {
+        imgsrc: "https://images.unsplash.com/photo-1590935216595-f9fc9b65179d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        title: "App Development"
+    },
+    {
+        imgsrc: "https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        title: "Software Development"
+    },
+    {
+        imgsrc: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        title: "Digital Marketing"
+    },
+    {
+        imgsrc: "https://images.unsplash.com/photo-1534665482403-a909d0d97c67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        title: "Android development"
+    },
+    {
+        imgsrc: "https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        title: "Marketing"
+    },
+]
+export default Sdata;
